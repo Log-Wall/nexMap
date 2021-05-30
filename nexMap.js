3103245cde7fac50385409409aa110ababd2cc88
Log-Wall <mrwallacelk@gmail.com>
2021-05-30 15:20:21 -0700

    Update nexMap.js

--- nexMap.js
+++ nexMap.js
@@ -877,3 +877,23 @@ nexMap.display.configDialog = function() {
         }
     });
 } 
+
+let generateStyle = function() {
+    let inject = function(rule) {
+        $('body').append('<div class="client_nexmap-rules">&shy;<style>' + rule + '</style></div>')   
+    };
+    if ( $('.client_nexmap-rules').length ) { 
+        $('.client_nexmap-rules').remove();
+    };
+    inject('.nexswitch {position: relative;display: inline-block;width: 38px;height: 22px;}'+
+        '.nexswitch input {opacity: 0;width: 0;height: 0;}'+
+    	'.nexslider {position: absolute;cursor: pointer;top: 0;left: 0;right: 0;bottom: 0;background-color: #ccc;-webkit-transition: .4s;transition: .4s;border-radius: 24px;}'+
+    	'.nexslider:before {position: absolute;content: "";height: 16px;width: 16px;left: 3px;bottom: 3px;background-color: white;-webkit-transition: .4s;transition: .4s;border-radius: 50%;}'+
+    	'input:checked + .nexslider {background-color: #2196F3;}'+
+    	'input:focus + .nexslider {box-shadow: 0 0 1px #2196F3;}'+
+    	'input:checked + .nexslider:before {-webkit-transform: translateX(16px);-ms-transform: translateX(16px);transform: translateX(16px);}'+
+        '.nexcontainer   { display: flex; }'+
+        '.nexfixed    { width: 200px; }'+
+        '.nexflex-item    { flex-grow: 1; }')
+};         
+generateStyle(); 
