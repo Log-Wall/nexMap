6b993245e318b1d8fc4f6a5dbbec0fe46e99690b
Log-Wall <mrwallacelk@gmail.com>
2021-05-31 19:06:28 -0700

    css updates

--- nexMap.js
+++ nexMap.js
@@ -620,7 +620,7 @@ nexMap.styles.style = function() {
         if ( $('.client_nexmap-rules').length ) { 
             $('.client_nexmap-rules').remove();
         };
-        inject('#tab_nexmap_map::before {content: "\\f262";}'+
+        let nexMapCSS = '#tab_nexmap_map::before {content: "\\f262";}'+
             '.nexswitch {position: relative;display: inline-block;width: 38px;height: 22px;}'+
             '.nexswitch input {opacity: 0;width: 0;height: 0;}'+
             '.nexslider {position: absolute;cursor: pointer;top: 0;left: 0;right: 0;bottom: 0;background-color: #555555;-webkit-transition: .4s;transition: .4s;border-radius: 24px;}'+
@@ -630,7 +630,11 @@ nexMap.styles.style = function() {
             'input:checked + .nexslider:before {-webkit-transform: translateX(16px);-ms-transform: translateX(16px);transform: translateX(16px);}'+
             '.nexcontainer   { display: flex; }'+
             '.nexfixed    { width: 200px; }'+
-            '.nexflex-item    { flex-grow: 1; }')
+            '.nexflex-item    { flex-grow: 1; }';
+        if (client.css_style!='standard')
+            nexMapCSS += '#tab_nexmap_map::before {content: "\\f262";}';
+            
+        inject(nexMapCSS);
     };         
     generateStyle(); 
 }
