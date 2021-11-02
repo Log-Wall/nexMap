b1f72f5ee8a900ac066f89849bf86d2a4e1b700e
Log-Wall <mrwallacelk@gmail.com>
2021-11-01 22:00:43 -0700

    minor

--- nexMap.js
+++ nexMap.js
@@ -219,7 +219,8 @@ var nexMap = {
               
         cy.endBatch();
         cy.center(`#${id}`); 
-        $('#currentRoomLabel').text(`${room.data('areaName')}: ${room.data('name')}`)
+        $('#currentRoomLabel').text(`${room.data('areaName')}: ${GMCP.Room.Info.num} (${GMCP.Room.Info.num})`)
+        //$('#currentRoomLabel').text(`${room.data('areaName')}: ${room.data('name')} (${GMCP.Room.Info.num})`)
         //$('#currentExitsLabel').text(`Exits: ${room.data('exits').join(', ')}`)
     
         $('.clickableExitSpace').remove();
